094264ee230713de31167a799f089fd6dda8b6a8
Zohaib Ahmad <workwithzhb@gmail.com>
2026-02-06 23:50:50 +0300

    Fix OneSignal init config

--- components/OneSignalInit.tsx
+++ components/OneSignalInit.tsx
@@ -10,9 +10,6 @@ export default function OneSignalInit() {
       await OneSignal.init({
         appId: "51a390df-a96d-4612-8468-3cd8848b8caf",
         allowLocalhostAsSecureOrigin: true,
-        notifyButton: {
-          enable: false,
-        },
       });
     };
 
