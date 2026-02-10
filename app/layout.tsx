cb80853cf720778e8b60d7f10927d3da0f2fabfb
Zohaib Ahmad <workwithzhb@gmail.com>
2026-02-10 10:35:56 +0300

    Updated metadata icons for PWA

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,18 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Traderlify",
   description: "Quotation, Invoice & Payment Follow-up Reminders",
+
+  manifest: "/manifest.json",
+
+  icons: {
+    icon: [
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/android-chrome-192x192.png", sizes: "192x192", type: "image/png" },
+      { url: "/android-chrome-512x512.png", sizes: "512x512", type: "image/png" }
+    ],
+    apple: "/apple-touch-icon.png",
+  },
 };
 
 export default function RootLayout({
